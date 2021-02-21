4a082c422d6e8f0d36406047892dae8e381cdcf7
Carlos Anzola <carloseduardoanzola93@gmail.com>
2021-02-21 17:30:31 -0500

    Se agrega mapas con capas

--- functions.py
+++ functions.py
@@ -3,63 +3,15 @@ import numpy as np
 import psycopg2
 import json
 import geopandas as gpd
+import folium
+import streamlit as st
+
+from folium import plugins
+from folium.plugins import HeatMap
 
 
 class Utilidades:
   
-
-  def cargar_csv(self, ruta_csv='datos.csv'):
-    '''
-        Carga datos de un archivo csv
-
-        :params:
-
-        ruta_csv : str
-              Es el path del archivo CSV
-        
-        :returns:
-
-        pd.DataFrame
-    '''
-    return pd.read_csv(ruta_csv)
-
-  def cargar_mapa(self, ruta='./mapas/manzana/MGN_URB_MANZANA.shp'):
-    '''
-        Carga mapa usando geopandas
-
-        :params:
-
-        ruta : str
-            Es el path del mapa
-
-        :returns:
-
-        gpd.DataFrame
-    '''
-
-    return gpd.read_file(ruta)
-
-  def depurar_columnas(self, gpd_mapa, codigo_dpto='50', columnas_a_dejar=['DPTO_CCDGO','MPIO_CCDGO','MANZ_CCNCT','geometry']):
-    '''
-        Filtra un archivo de mapa por su departamento y deja las columnas deseadas
-
-        :params:
-        
-        gpd_mapa : gpd.DataFrame
-            Es un objeto cargado con geopandas que se quiere depurar
-        
-        columnas_a_dejar : list
-            Es el listado de columnas que se quieren dejar en el dataframe
-        
-        :returns:
-
-        gpd.DataFrame
-
-    '''
-    return gpd_mapa[gpd_mapa['DPTO_CCDGO'] == codigo_dpto][columnas_a_dejar]
-
-
-
   def conectar_db(self):
     '''
         Hace la conexión a la base de datos postgres donde se encuentra la información descargado del censo nacional de colombia 2018 alojada en AWS,
@@ -76,7 +28,7 @@ class Utilidades:
     conn= psycopg2.connect(dbname=credenciales['DB_NAME'],user=credenciales['db_user'],password=credenciales['db_pass'], host= credenciales['host_aws'])
     return conn
 
-
+  
   def cargar_datos_db(self,conexion, query='''SELECT * FROM resumen_features_manzanas'''):
     '''
         Hace una consulta a una base de datos y regresa un pd.DataFrame con la informacion
@@ -97,10 +49,107 @@ class Utilidades:
     datos = pd.read_sql(query, con=conexion)
     return datos
 
+  
+  def cargar_csv(self, ruta_csv='datos.csv'):
+    '''
+        Carga datos de un archivo csv
+
+        :params:
+
+        ruta_csv : str
+              Es el path del archivo CSV
+        
+        :returns:
+
+        pd.DataFrame
+    '''
+    return pd.read_csv(ruta_csv)
+
+  
+  def cargar_mapa(self, ruta='./mapas/manzana/MGN_URB_MANZANA.shp'):
+    '''
+        Carga mapa usando geopandas
+
+        :params:
+
+        ruta : str
+            Es el path del mapa
+
+        :returns:
+
+        gpd.DataFrame
+    '''
+
+    return gpd.read_file(ruta)
+
+  
+  def depurar_columnas(self, gpd_mapa, codigo_dpto='50', columnas_a_dejar=['DPTO_CCDGO','MPIO_CCDGO','MANZ_CCNCT','geometry']):
+    '''
+        Filtra un archivo de mapa por su departamento y deja las columnas deseadas
+
+        :params:
+        
+        gpd_mapa : gpd.DataFrame
+            Es un objeto cargado con geopandas que se quiere depurar
+        
+        columnas_a_dejar : list
+            Es el listado de columnas que se quieren dejar en el dataframe
+        
+        :returns:
+
+        gpd.DataFrame
+
+    '''
+    return gpd_mapa[gpd_mapa['DPTO_CCDGO'] == codigo_dpto][columnas_a_dejar]
+
+  
+  def unir_dataframes(self, df_izquierda, indice_izquierda, df_derecha, indice_derecha, tipo_union='left'):
+    '''
+        Une dos dataframes
+
+        :params:
+
+        tipo_union : str
+            Es el tipo de union que se realiza, por default toma left
+
+        df_izquierda : pd.DataFrame
+            Es el dataframe al cual se le hara un merge
+        
+        indice_izquierda : str
+            Feature(s) o columna(s) por la cual se hara la union en la izquierda
+
+        df_derecha : pd.DataFrame
+            Es el dataframe que se unira
+        
+        indice_derecha : str
+            Feature(s) o columna(s) por la cual se hara la union en la derecha
+
+        :returns:
+
+        final : pd.DataFrame
+            Dataframe con los datos de la union 
+    '''
+    df_unido = df_izquierda.merge(df_derecha, how=tipo_union, left_on=indice_izquierda, right_on=indice_derecha)
+    return df_unido
 
 
+class Mapa(folium.Map):
 
-
-
-
-
+  
+  def __init_(self, *args, **kwargs):
+    folium.Map.__init__(*args, **kwargs)
+  
+  
+  def agregar_capa(self, nombre_capa, gpd_mapa, df_datos, columnas_datos, nombre_leyenda, llave_mapa='feature.properties.MANZ_CCNCT'):
+    capa=folium.Choropleth(
+      geo_data=gpd_mapa,
+      name=nombre_capa,
+      data=df_datos,
+      columns=columnas_datos,
+      key_on=llave_mapa,
+      fill_color="YlOrRd",
+      fill_opacity=0.7,
+      line_opacity=0.9,
+      legend_name=nombre_leyenda
+  ).add_to(self)
+    
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import pandas as pd
+import pandas as pd 
+import numpy as np
+import psycopg2
+import json
 import geopandas as gpd
+import folium
+import streamlit as st
+
+from streamlit_folium import folium_static
+from folium import plugins
+from folium.plugins import HeatMap
 
 
 from functions import Utilidades
+from functions import Mapa
 
-
-def main():
+@st.cache(suppress_st_warning=True)
+def cargue():
 
     utils = Utilidades()
 
@@ -16,14 +26,30 @@ def main():
 
     datos_mapas = utils.cargar_mapa()
     datos_mapas = utils.depurar_columnas(gpd_mapa=datos_mapas)
-    
-    print(data.head())
-    print(datos_mapas.head())
 
-    final = data.merge(datos_mapas[['MANZ_CCNCT','geometry']], how='left', left_on='codigo_manzana', right_on='MANZ_CCNCT')
-    print(final.head())
-    print(final.info())
+    final = utils.unir_dataframes(df_izquierda=data, indice_izquierda='codigo_manzana', df_derecha=datos_mapas[['MANZ_CCNCT','geometry']], indice_derecha='MANZ_CCNCT')
+    
+    return datos_mapas, final
+
+@st.cache(suppress_st_warning=True)
+def dibujar_mapa():
+  mapa = Mapa(location=[4.05, -73.627321], zoom_start=7)
+  mapa.agregar_capa(nombre_capa='Estrato', gpd_mapa=datos_mapas, df_datos=final, columnas_datos=['MANZ_CCNCT','estrato'], nombre_leyenda='Estrato')  
+  folium.LayerControl().add_to(mapa)
+  return mapa
 
 
 if __name__ == '__main__':
-  main()
+  datos_mapas, final = cargue()
+  
+  st.title('Prueba de streamlit')
+  
+  menu = ["Opcion1","Opcion2","Opcion3"]
+  
+  choice = st.sidebar.selectbox('Menu',menu)
+  
+  if choice == "Opcion1":
+    st.subheader('Opcion1')
+  else:
+    pass
+  
